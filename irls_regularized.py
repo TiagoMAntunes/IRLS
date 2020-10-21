1fbd6e2604908ac1633e5b8f6a70fa87b6d14196
TiagoMAntunes <tiago.melo.antunes@gmail.com>
2020-10-21 22:20:01 +0100

    Restructured irls. Parser now using lib

--- irls_regularized.py
+++ irls_regularized.py
@@ -1,79 +1,122 @@
 import numpy as np
 from parser import Parser
 from scipy.special import expit
-
-
+from sklearn.model_selection import KFold
+import scipy.sparse as sparse
+from pprint import pprint
+import pickle 
 class RegularizedIRLS:
     def __init__(self, msteps=100):
         self.maxsteps = msteps
         self.models = []
 
-    def result(self, v):
-        return 1 if v >= 0.5 else 0
-
     def hypothesis(self, x,w): #sigmoid
         val = w.T.dot(x)
-        return 1.0/(1+np.exp(-val))
+        return 1.0/(1+np.exp(-val)) # if an np array is used it will return an array instead of a scalar
 
-    def test(self, filename, W):
-        p = Parser(filename)
-        X = p.transform_x()
-        Y = p.transform_y()
+    def test(self, W, X, Y):
+        """ Return the accuracy of the given weights over the given test """
+        guesses = np.array(list(map(lambda x: 1 if x >= 0.5 else 0, self.hypothesis(X,W))))
+        return np.mean(guesses == Y)
 
-        t_success = 0
-        val_success = [0,0]
-        count = [0,0]
-        for i in range(len(X)):
-            yi = Y[i]
-            xi = X[i]
-            res = self.result(self.hypothesis(xi, W))
-            if res == yi:
-                t_success += 1 
-                val_success[yi] += 1
-            count[yi] += 1
-                
-        # print ('Correct: {}'.format(t_success * 100 / len(X)))
-        # print ('Positive correct {}'.format(val_success[1] * 100 / count[1]))
-        # print ('Negative correct {}'.format(val_success[0] * 100 / count[0]))
-
-
-    def train(self, filename, regularization, threshold=10**-5):
-        p = Parser(filename)
-        X = p.transform_x().T # data points 
-        Y = p.transform_y() # results
-
-        W = np.zeros(124)
+    def train(self, parserinput, regularization, threshold=5):
         
-        #training
-        diff = [0, 1]
-        steps = 0
-        while np.abs(diff[1] - diff[0]) > threshold and steps < self.maxsteps:
-            steps+=1
-            #no need to calculate R
-            # ith row of X multiplies by Rii
-            R = np.array([self.hypothesis(xi, W)*(1 - self.hypothesis(xi, W)) for xi in X.T])
+        def train_model(X,Y, regularization):
+            """ Trains a model given input X, labels Y and with a given regularization """
+            W = np.zeros(124)
             
-            predictions = self.hypothesis(X, W)
-            
-            hessian = np.linalg.pinv(regularization * np.identity(124) + (X * R).dot(X.T))
-            new_W = hessian.dot((X*R).dot(X.T).dot(W) + X.dot(Y - predictions))
-            
-            diff[0] = diff[1]
-            diff[1] = np.linalg.norm(W - (-regularization*W + X.dot(Y-predictions)))
-            
-            W = new_W
+            sparse_X = sparse.csr_matrix(X.T)
 
-            
-        self.models.append((len(self.models), W, steps))
+            #training
+            diff = [0, 1]
+            steps = 0
+            max_acc_count = 0
+            best_accuracy = 0
+            stop_condition = threshold
+            while max_acc_count < stop_condition and steps < self.maxsteps:
+                steps+=1
+
+                #no need to calculate total R
+                # ith row of X multiplies by Rii
+                miu = self.hypothesis(X,W) 
+                R = np.array(miu*(1-miu))
+                
+                #common = (X * R) @ X.T
+                common = sparse.csr_matrix(X*R).dot(sparse_X).toarray()
+                
+                predictions = miu
+
+                hessian = np.linalg.pinv(regularization * np.identity(124) + common)
+                new_W = hessian.dot(common.dot(W) + X.dot(Y - predictions))
+                
+                # diff[0] = diff[1]
+                # diff[1] = np.linalg.norm(W - (-regularization*W + X.dot(Y-predictions))) # difference in gradient
+                # print('[DEBUG] diff is {}'.format(np.abs(diff[1] - diff[0])))
+                accuracy = self.test(W,X,Y)
+                if accuracy > best_accuracy:
+                    best_accuracy = accuracy
+                    max_acc_count = 0
+                else:
+                    max_acc_count += 1
+
+                W = new_W
+            return steps, W
+
+
+        def fold_train(X,Y,N, regularization):
+            """ Applies training over N foldings"""
+
+            kf = KFold(n_splits=N)
+            models = []
+            counter = 1
+            for train_index, test_index in kf.split(X):
+                X_train, X_test = X[train_index], X[test_index]
+                Y_train, Y_test = Y[train_index], Y[test_index]
+                steps, W = train_model(X_train.T, Y_train, regularization)
+                models.append((counter,steps,W))
+                counter+=1
+
+            return models
+        
+        p = parserinput
+        X = p.transform_x() # data points 
+        Y = p.transform_y() # labels
+        
+        steps, W = train_model(X.T,Y, regularization)
+        print('[DEBUG] Trained without foldings')
+        fold_res = ((0, steps, W), *fold_train(X,Y,2,regularization)) # 0 is always the non folded version
+        print('[DEBUG] Folded training complete')
+        self.models.append({'regularization': regularization, 'models': fold_res})
         return W
-
-# regularization = 0.001
-# model = train('a9a', regularization)
 
 if __name__ == '__main__':
     m = RegularizedIRLS()
-    models = [m.train('a9a', r) for r in (1, 5, 10)]
-    for model in models:
-        m.test('a9a.t', model)
-        m.test('a9a', model)
+    
+    pp = Parser('a9a')
+    X = pp.transform_x()
+    Y = pp.transform_y()
+    for r in (0,0.001,0.003,0.01,0.03, 0.1,0.3,*range(1,100)):
+        print('Starting train... r={}'.format(r))
+        m.train(pp, r)
+        print('Training finished')
+    
+    p = Parser('a9a.t')
+    X_test = p.transform_x()
+    Y_test = p.transform_y()
 
+    #print(m.models)
+    
+    accuracies = [] # training, test
+    for model in m.models:
+        tmp = []
+        for x,y in ((X.T,Y), (X_test.T, Y_test)):
+            non_folded_test = m.test(model['models'][0][2], x,y)
+            #w = sum(list(map(lambda m: m[2], model['models'][1:]))) / len(model['models'][1:])
+            vals = [m.test(w[2],x,y) for w in model['models']]
+            tmp.append((non_folded_test,np.mean(vals)))
+        accuracies.append(tmp)
+            
+    
+    resobj = {'acc': accuracies, 'models': m.models}
+    pickle.dump(resobj, open('resultsobj', 'wb'))
+    
--- parser.py
+++ parser.py
@@ -1,30 +1,18 @@
 import numpy as np
+from sklearn.datasets import load_svmlight_file
 
 class Parser:
     def __init__(self, target):
-        with open(target) as f:
-            self._lines = f.readlines()
-    
-    def parse(self):
-        if not hasattr(self, '_data'):
-            f = lambda a: (int(a[0]), list(map(lambda x: (int(x[0]),int(x[1])), map(lambda x: x.split(':'), a[1:]))))
-            self._data = [f(x.split()) for x in self._lines]
-        return self._data
+        self._data = load_svmlight_file(target, n_features=123)
 
     def transform_x(self):
-        self.parse()
-        contents = list(map(lambda x: x[1], self._data))
-        size = 124
-        def transform(l):
-            line = [0 for i in range(size)]
-            line[0] = 1
-            for i in l:
-                line[i[0]] = i[1]
-            return line
-        p = list(map(transform, contents))
-        return np.array(p)
+        X, _ = self._data
+        X = X.toarray()
+        v = np.ones(X.shape[0])
+        p = np.vstack((v, X.T)).T
+        return p
 
     def transform_y(self):
-        self.parse()
-        contents = list(map(lambda x: x[0], self._data))
-        return (np.array(contents) + 1) // 2 # transform into {0, 1}
+        # contents = list(map(lambda x: x[0], self._data))
+        _, Y = self._data
+        return (Y + 1) // 2 # transform into {0, 1}
